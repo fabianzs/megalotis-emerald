f5dddb8fdb229e00e5aecb47138b08ea36a17d74
Szirmai, Attila <szirmi@gmail.com>
2019-04-12 08:15:51 +0200

    initial commit

---
+++ ASP.NET-Core-Webapp/Startup.cs
@@ -0,0 +1,26 @@
+﻿using ASP.NET_Core_Webapp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASP.NET_Core_Webapp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<IHelloService, HelloService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
