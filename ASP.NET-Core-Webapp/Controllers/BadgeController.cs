8fd8732b5a33a760bf183f38930e871459fb1f23
JohnLacklan <janos.f96@gmail.com>
2019-04-15 16:29:51 +0200

    Make mybadges endpoint

---
+++ ASP.NET-Core-Webapp/Controllers/BadgeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using ASP.NET_Core_Webapp.Entities;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Webapp.Controllers
+{
+    public class BadgeController : Controller
+    {
+        [HttpGet("mybadges")]
+        public IActionResult MyBadges()
+        {
+            User user = new User();
+
+            List<Badge> badges = new List<Badge>();
+
+            badges.Add(new Badge("test", 4));
+            badges.Add(new Badge("test", 4));
+            badges.Add(new Badge("test", 4));
+
+            return Ok(new {badges = badges });
+            
+        }
+
+        
+    }
+}
---
+++ ASP.NET-Core-Webapp/Entities/Badge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Webapp.Entities
+{
+    public class Badge
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public Badge(string name, int level)
+        {
+            this.Name = name;
+            this.Level = level;
+        }
+    }
+}
