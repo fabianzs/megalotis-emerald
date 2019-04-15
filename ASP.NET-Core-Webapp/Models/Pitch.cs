f4ad31745f1e0ca2b97c6557c80051a535841bb8
Botond Balogh <botond.balogh8@gmail.com>
2019-04-15 11:43:29 +0200

    Create models

---
+++ ASP.NET-Core-Webapp/Models/Pitch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Webapp.Models
+{
+    public class Pitch
+    {
+
+        public class Rootobject
+        {
+            public string Timestamp { get; set; }
+            public string Username { get; set; }
+            public string BadgeName { get; set; }
+            public int OldLevel { get; set; }
+            public int PitchedLevel { get; set; }
+            public string PitchMessage { get; set; }
+            public Holder[] Holders { get; set; }
+        }
+
+        public class Holder
+        {
+            public string Name { get; set; }
+            public object Message { get; set; }
+            public bool PitchStatus { get; set; }
+        }
+
+    }
+}
