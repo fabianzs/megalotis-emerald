867c48b8a597c47b46f38321e00b5fea62d4f0f1
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-16 15:17:51 +0200

    Add multiple test cases for badges endpoint

--- ASP.NET-Core-Webapp.IntegrationTests/Scenarios/MockPostBadgesEndpointTest.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/MockPostBadgesEndpointTest.cs
@@ -1,6 +1,10 @@
-﻿using ASP.NET_Core_Webapp.IntegrationTests.Fixtures;
+﻿using ASP.NET_Core_Webapp.Entities;
+using ASP.NET_Core_Webapp.IntegrationTests.Fixtures;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,11 +21,42 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
         }
 
         [Fact]
-        public async Task MockPostBadges_Should_Return_404()
+        public async Task MockPostBadges_LevelsFieldMissing_ShouldReturn404()
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "/badges?authorized=1");
-            var response = await testContext.Client.SendAsync(request);
+
+            var response = await testContext.Client.PostAsync("/badges?authorized=1", new StringContent(JsonConvert.SerializeObject(new Badge() { Name = "test", Tag = "test", Version = "test", Levels = null }), Encoding.UTF8, "application/json"));
+
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task MockPostBadges_NoBodyContent_ShouldReturn404()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "/badges?authorized=1");
+
+            var response = await testContext.Client.PostAsync("/badges?authorized=1", new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task MockPostBadges_AllFielsdFilled_ShouldReturn201()
+        {
+
+            List<string> holders = new List<string>() { "Laci", "Gábor", "Levente" };
+
+            Badge badge = new Badge() { Name = "test", Tag = "test", Version = "test", Levels = new List<LevelEntity>() };
+
+            LevelEntity testlevel = new LevelEntity() {Description = "test", Holders = holders, Level = 2 };
+
+            badge.Levels.Add(testlevel);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "/badges?authorized=1");
+
+            var response = await testContext.Client.PostAsync("/badges?authorized=1", new StringContent(JsonConvert.SerializeObject(badge), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
 
 
