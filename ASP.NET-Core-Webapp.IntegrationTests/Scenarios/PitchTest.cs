262233329ff6135cfeb546f8b0ddfca8e5edbdf7
Botond Balogh <botond.balogh8@gmail.com>
2019-04-17 14:58:52 +0200

    Comment test

--- ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchTest.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchTest.cs
@@ -48,14 +48,14 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
         }
 
         [Fact]
-        public async Task Pitch_UserNotNull_Success()
+        public async Task Pitch_UserNotNull_NotEqual()
         {
             var request = new HttpRequestMessage(HttpMethod.Get, "/pitches");
             var response = await testContext.Client.SendAsync(request);
 
             response.EnsureSuccessStatusCode();
 
-            Assert.NotEqual(new StringContent(JsonConvert.SerializeObject(new User())).ToString().ToCharArray(), response.Content.ReadAsStringAsync().Result);
+            Assert.Equal(new StringContent(JsonConvert.SerializeObject(new User()), Encoding.UTF8, "application/json").ToString(), response.Content.ReadAsStringAsync().Result);
             //Assert.NotEqual("{\"myPitches\":null,\"pitchesToReview\":null}", response.Content.ReadAsStringAsync().Result);
 
         }
