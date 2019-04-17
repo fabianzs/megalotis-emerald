8ddc3cb868295adf0ff921e618cdf0e304250ab0
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-17 15:55:07 +0200

    Configure Startup.cs for authentication

--- ASP.NET-Core-Webapp/Startup.cs
+++ ASP.NET-Core-Webapp/Startup.cs
@@ -1,16 +1,53 @@
-﻿using ASP.NET_Core_Webapp.Services;
+﻿using ASP.NET_Core_Webapp.Helpers;
+using ASP.NET_Core_Webapp.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace ASP.NET_Core_Webapp
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
             services.AddScoped<IHelloService, HelloService>();
+
+            services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+            services.AddAuthorization(auth =>
+                    {
+                        auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
+                            .RequireAuthenticatedUser().Build());
+                    });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+                            ValidateIssuerSigningKey = true,
+                            ValidateLifetime = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings").Get<AppSettings>().Secret)),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -20,7 +57,18 @@ namespace ASP.NET_Core_Webapp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Auth}/{action=Login}");
+                });
+
+            app.UseAuthentication();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
         }
     }
 }
