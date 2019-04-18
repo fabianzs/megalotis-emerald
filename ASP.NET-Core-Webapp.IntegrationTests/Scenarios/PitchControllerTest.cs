80581bde47584de701bd74b3ded1c37cc83b7618
osziszab <osztertagszabolcs@gmail.com>
2019-04-18 10:36:38 +0200

    testing finished and working fine

--- ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchControllerTest.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchControllerTest.cs
@@ -39,19 +39,17 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
 
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
-/*
+
         [Fact]
         public async Task CreateNewPitchPitchAlreadyExist()
         {
-            Pitch pitch = new Pitch() { BadgeName = "English speaker", OldLevel = 2, PitchedLevel = 3, PitchMessage = "Hello World! My English is bloody gorgeous.", Holders = { "balazs.jozsef", "benedek.vamosi", "balazs.barna" } };
+            Pitch pitch = new Pitch() { BadgeName = "C# pro", OldLevel = 2, PitchedLevel = 3, PitchMessage = "Hello World! My English is bloody gorgeous.", Holders = { "balazs.jozsef", "benedek.vamosi", "balazs.barna" } };
 
             var request = new HttpRequestMessage(HttpMethod.Post, "/api/pitches");
             var response = await testContext.Client.PostAsync("/api/pitches", new StringContent(JsonConvert.SerializeObject(pitch), Encoding.UTF8, "application/json"));
 
-           request = new HttpRequestMessage(HttpMethod.Post, "/api/pitches");
-           response = await testContext.Client.PostAsync("/api/pitches", new StringContent(JsonConvert.SerializeObject(pitch), Encoding.UTF8, "application/json"));
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
-*/
+
     }
 }
