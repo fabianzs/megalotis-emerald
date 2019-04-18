5a7eb8a03fa0fde098753f90cb7003bf0ed7d1aa
JohnLacklan <janos.f96@gmail.com>
2019-04-18 11:09:46 +0200

    Save all

--- ASP.NET-Core-Webapp/Entities/Badge.cs
+++ ASP.NET-Core-Webapp/Entities/Badge.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,36 +9,19 @@ namespace ASP.NET_Core_Webapp.Entities
     public class Badge
     {
         public string Name { get; set; }
-        public int Level { get; set; }
-        public string Tag { get; set; }
-        public string Version { get; set; }
-        
-
-        public Badge(string name, int level)
-        {
-            this.Name = name;
-            this.Level = level;
-        }
-    }
-}
-=======
-﻿using System.Collections.Generic;
-
-namespace ASP.NET_Core_Webapp.Entities
-{
-
-    public class Badge
-    {
-        public string Version { get; set; }
-        public string Name { get; set; }
-        public string Tag { get; set; }
         public ICollection<LevelEntity> Levels { get; set; }
+        public string Tag { get; set; }
+        public string Version { get; set; }
 
         public Badge()
         {
             this.Levels = new List<LevelEntity>();
         }
 
+        public Badge(string name)
+        {
+            this.Name = name;
+            this.Levels = new List<LevelEntity>();
+        }
     }
 }
->>>>>>> origin/Mock_Post_Badges_Endpoint
--- ASP.NET-Core-Webapp/Entities/LevelEntity.cs
+++ ASP.NET-Core-Webapp/Entities/LevelEntity.cs
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-=======
+
 ﻿using System.Collections.Generic;
->>>>>>> origin/Mock_Post_Badges_Endpoint
 
 namespace ASP.NET_Core_Webapp.Entities
 {
     public class LevelEntity
     {
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/Mock_Post_Badges_Endpoint
         public int Level { get; set; }
         public string Description { get; set; }
         public ICollection<string> Holders { get; set; }
