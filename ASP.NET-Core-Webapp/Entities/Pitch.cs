eb9eac646d28842fad06d2e00ebdc84f9a9d8878
JohnLacklan <janos.f96@gmail.com>
2019-04-24 14:37:27 +0200

    Fix Pitch fields

--- ASP.NET-Core-Webapp/Entities/Pitch.cs
+++ ASP.NET-Core-Webapp/Entities/Pitch.cs
@@ -7,17 +7,22 @@ namespace ASP.NET_Core_Webapp.Entities
 {
     public class Pitch
     {
-        public DateTime Timestamp { get; set; }
-        public string Username { get; set; }
-        public string BadgeName { get; set; }
-        public int OldLevel { get; set; }
+        public long PitchId { get; set; }
+        public DateTime TimeStamp { get; set; }
         public int PitchedLevel { get; set; }
         public string PitchMessage { get; set; }
+<<<<<<< HEAD
 <<<<<<< HEAD
         public List<string> Holders { get; set; }
 
         public Pitch()
 =======
+=======
+        public User User { get; set; }
+        public Badge Badge { get; set; }
+        public BadgeLevel BadgeLevel { get; set; }
+
+>>>>>>> be1db76... Fix Pitch fields
         public ICollection<Review> Holders { get; set; }
 
         public Pitch(string username, string badgeName, int oldLevel, int pitchedLevel, string pitchMessage, ICollection<Review> holders)
