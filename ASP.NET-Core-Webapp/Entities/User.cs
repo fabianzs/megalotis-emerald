1e320b2f3dd3a8509074966977e825626973de58
JohnLacklan <janos.f96@gmail.com>
2019-04-24 14:39:48 +0200

    Fix Pitch fields

---
+++ ASP.NET-Core-Webapp/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ASP.NET_Core_Webapp.Entities
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public string MyProperty { get; set; }
+        public string Email { get; set; }
+        public string OpenId { get; set; }
+
+        public User(ICollection<Pitch> myPitches, ICollection<Pitch> pitchesToReview)
+        {
+            this.myPitches = myPitches;
+            this.pitchesToReview = pitchesToReview;
+        }
+
+        public User()
+        {
+
+        }
+    }
+}
