ab42eba3ce793602ba021a951c4948a3e036fbcd
JohnLacklan <janos.f96@gmail.com>
2019-04-24 13:36:57 +0200

    Add AplicationContext 2

--- ASP.NET-Core-Webapp/Data/ApplicationContext.cs
+++ ASP.NET-Core-Webapp/Data/ApplicationContext.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using ASP.NET_Core_Webapp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Webapp.Data
 {
-    public class ApplicationContext
+    public class ApplicationContext : DbContext
     {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Pitch> Pitches { get; set; }
+        public DbSet<BadgeLevel> BadgeLevels { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
