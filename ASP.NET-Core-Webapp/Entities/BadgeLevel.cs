22fd32ab7f45f3b623b101f8e971cd722ecd6e83
JohnLacklan <janos.f96@gmail.com>
2019-04-24 14:36:37 +0200

    Fix BadgeLevel fields

---
+++ ASP.NET-Core-Webapp/Entities/BadgeLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ASP.NET_Core_Webapp.Entities
+{
+    public class BadgeLevel
+    {
+        public long BadgeLevelId { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public Badge Badge { get; set; }
+        public ICollection<User> Holders { get; set; }
+    }
+}
