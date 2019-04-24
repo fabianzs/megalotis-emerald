be1ca4be990e17b6134635a4007fdb26e745c4dd
Botond Balogh <botond.balogh8@gmail.com>
2019-04-24 14:07:52 +0200

    Custom error message

--- ASP.NET-Core-Webapp/Startup.cs
+++ ASP.NET-Core-Webapp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Webapp
 {
@@ -46,6 +48,22 @@ namespace ASP.NET_Core_Webapp
                             ValidateLifetime = true,
                             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["Authentication:Jwt:Secret"])),
                             ClockSkew = TimeSpan.Zero
+                        };
+                        options.Events = new JwtBearerEvents()
+                        {
+                            OnAuthenticationFailed = c =>
+                            {
+                                c.NoResult();
+                                c.Response.StatusCode = 401;
+                                c.Response.ContentType = "application/json";
+                                c.Response.WriteAsync(JsonConvert.SerializeObject(new CustomErrorMessage("Unauthorized"))).Wait();
+                                return Task.CompletedTask;
+                            },
+                            OnChallenge = c =>
+                            {
+                                c.HandleResponse();
+                                return Task.CompletedTask;
+                            }
                         };
                     });
 
