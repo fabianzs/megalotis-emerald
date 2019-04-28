9a7e9f3268cbf59fc36ea33cf03b7fbc64ab62e0
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-04-28 21:53:42 +0200

    DTO entities for POST /badges endpoint create

---
+++ ASP.NET-Core-Webapp/Entities/BadgeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Webapp.Entities
+{
+    public class BadgeDTO
+    {
+        public long BadgeId { get; set; }
+        public string Version { get; set; }
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public ICollection<BadgeLevelDTO> Levels { get; set; }
+    }
+}
---
+++ ASP.NET-Core-Webapp/Entities/BadgeLevelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Webapp.Entities
+{
+    public class BadgeLevelDTO
+    {
+        public long BadgeLevelId { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public Badge Badge { get; set; }
+        public virtual ICollection<String> Holders { get; set; }
+    }
+}
