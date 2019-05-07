aa6227d397783faed363bdce19eb586aa1f4a599
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-05-07 17:53:56 +0200

    Remove outcommented rows

--- ASP.NET-Core-Webapp/Controllers/AuthController.cs
+++ ASP.NET-Core-Webapp/Controllers/AuthController.cs
@@ -1,47 +1,47 @@
-﻿//using ASP.NET_Core_Webapp.Helpers;
-//using ASP.NET_Core_Webapp.Services;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_Core_Webapp.Helpers;
+using ASP.NET_Core_Webapp.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ASP.NET_Core_Webapp.Controllers
-//{
-//    public class AuthController : Controller
-//    {
-//        private readonly IAuthService authService;
+namespace ASP.NET_Core_Webapp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService authService;
 
-//        public AuthController(IAuthService authService)
-//        {
-//            this.authService = authService;
-//        }
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
 
-//        [HttpGet("")]
-//        public IActionResult Login()
-//        {
-//            return Redirect(authService.GetGoogleLogin());
-//        }
+        [HttpGet("")]
+        public IActionResult Login()
+        {
+            return Redirect(authService.GetGoogleLogin());
+        }
 
-//        [HttpGet("auth")]
-//        public IActionResult Authenticate(string code)
-//        {
-//            GoogleToken token = authService.GetToken(code);
-//            TokenInfo tokenInfo = authService.ValidateToken(token.id_token);
-//            bool isValid = tokenInfo.email_verified;
-//            if (isValid)
-//            {
-//                var tokenstring = authService.CreateJwtToken(tokenInfo.sub, tokenInfo.email);
-//                return Ok(tokenstring);
-//            }
-//            else
-//            {
-//                return BadRequest();
-//            }
-//        }
+        [HttpGet("auth")]
+        public IActionResult Authenticate(string code)
+        {
+            GoogleToken token = authService.GetToken(code);
+            TokenInfo tokenInfo = authService.ValidateToken(token.id_token);
+            bool isValid = tokenInfo.email_verified;
+            if (isValid)
+            {
+                var tokenstring = authService.CreateJwtToken(tokenInfo.sub, tokenInfo.email);
+                return Ok(tokenstring);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
 
-//        [Authorize("Bearer")]
-//        [HttpGet("test")]
-//        public IActionResult Test()
-//        {
-//            return Ok("You are authorized");
-//        }
-//    }
-//}
+        [Authorize("Bearer")]
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("You are authorized");
+        }
+    }
+}
