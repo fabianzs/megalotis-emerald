0ea9cd47914de7af1a7488d8d2d6aa212cfe9678
JohnLacklan <janos.f96@gmail.com>
2019-05-07 17:41:49 +0200

    Make the sheet append in response

--- ASP.NET-Core-Webapp/Controllers/SpreadSheetController.cs
+++ ASP.NET-Core-Webapp/Controllers/SpreadSheetController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +12,17 @@ namespace ASP.NET_Core_Webapp.Controllers
         [HttpGet("spreadsheet")]
         public Object SpreadSheet()
         {
-           // Console.WriteLine(ReadAccesTokenFromFile());
             return ReadAccesTokenFromFile();
+        }
+
+        [HttpGet("spreadsheet2")]
+        public object SpreadSheet2()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Get, "https://sheets.googleapis.com/v4/spreadsheets/1oW7QEQVR_-aX3UYgN2n03UUB2GDAFqhE_DPRgPuukAI/values/Munkalap1?access_token=" + ReadAccesTokenFromFile());
+            HttpResponseMessage response = new HttpClient().SendAsync(req).Result;
+            string res = response.Content.ReadAsStringAsync().Result;
+
+            return res;
         }
 
         private string ReadAccesTokenFromFile()
