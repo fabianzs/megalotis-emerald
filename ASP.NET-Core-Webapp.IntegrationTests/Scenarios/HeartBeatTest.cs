03e989e43c40ff145d964a8ef129f8f5845abe70
Botond Balogh <botond.balogh8@gmail.com>
2019-05-14 15:37:04 +0200

    Set SeedData.json to copy if newer

--- ASP.NET-Core-Webapp.IntegrationTests/Scenarios/HeartBeatTest.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/HeartBeatTest.cs
@@ -9,21 +9,7 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
     [Collection("BaseCollection")]
     public class HeartBeatTest
     {
-        //private IUSD_CLP_ExchangeRateFeed prvGetMockExchangeRateFeed()
-        //{
-        //    Mock<IUSD_CLP_ExchangeRateFeed> mockObject = new Mock<IUSD_CLP_ExchangeRateFeed>();
-        //    mockObject.Setup(m => m.GetActualUSDValue()).Returns(500);
-        //    return mockObject.Object;
-        //}
-
-        //private IAuthService AuthService()
-        //{
-        //    Mock<IAuthService> mockObject = new Mock<IAuthService>();
-        //    return mockObject.Object;
-        //}
-
         private readonly TestContext testContext;
-
 
         public HeartBeatTest(TestContext testContext)
         {
@@ -35,10 +21,7 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
         {
             var request = new HttpRequestMessage(HttpMethod.Get, "heartbeat");
             var response = await testContext.Client.SendAsync(request);
-
-            //var response = await testContext.Client.GetAsync("heartbeat",);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            //Assert.True(true);0
         }
     }
 }
