9289465b83be4d3335d338a6d8d65456ec49c11b
Botond Balogh <botond.balogh8@gmail.com>
2019-05-15 10:13:22 +0200

    Add extra unit tests

--- ASP.NET-Core-Webapp.UnitTests/EmptyTest.cs
+++ ASP.NET-Core-Webapp.UnitTests/EmptyTest.cs
@@ -14,5 +14,21 @@ namespace ASP.NET_Core_Webapp.UnitTests
             int result = 5;
             Assert.Equal(result, n);
         }
+
+        [Fact]
+        public void Simpletest2()
+        {
+            int n = 0;
+            int result = 0;
+            Assert.Equal(result, n);
+        }
+
+        [Fact]
+        public void Simpletest3()
+        {
+            int n = 15;
+            int result = 15;
+            Assert.Equal(result, n);
+        }
     }
 }
