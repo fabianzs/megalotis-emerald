121d74467ac05075b863cdcd4ee13912508f42d4
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-15 03:21:59 +0200

    Add tests to PostReview endpoint, PostReviewTest.cs create, ReviewService.cs modify

---
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PostReviewTest.cs
@@ -0,0 +1,56 @@
+﻿using ASP.NET_Core_Webapp.DTO;
+using ASP.NET_Core_Webapp.IntegrationTests.Fixtures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
+{
+    [Collection("BaseCollection")]
+    public class PostReviewTest
+    {
+        private readonly TestContext testContext;
+
+        public PostReviewTest(TestContext testContext)
+        {
+            this.testContext = testContext;
+        }
+
+        [Fact]
+        public async Task PostReview_Should_Return_Ok()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "/review");
+            request.Content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PostReview_NoBodyContent_ShouldReturn404()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "/review");
+            request.Content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PostReview_AllFielsdFilled_ShouldReturn201()
+        {
+            ReviewDTO review = new ReviewDTO() { Message = "testreview", Status = true, PitchId = 4 };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "/review");
+            request.Content = (new StringContent(JsonConvert.SerializeObject(review), Encoding.UTF8, "application/json"));
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+    }
+}
--- ASP.NET-Core-Webapp/Services/ReviewService.cs
+++ ASP.NET-Core-Webapp/Services/ReviewService.cs
@@ -1,10 +1,9 @@
 ﻿using ASP.NET_Core_Webapp.Data;
 using ASP.NET_Core_Webapp.DTO;
 using ASP.NET_Core_Webapp.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Webapp.Services
 {
@@ -19,7 +18,26 @@ namespace ASP.NET_Core_Webapp.Services
 
         public void CreateReview(string openId, ReviewDTO reviewDTO)
         {
-            Review newReview = new Review() { Message = reviewDTO.Message, Status = reviewDTO.Status, Pitch = applicationContext.Pitches.FirstOrDefault(p => p.PitchId == reviewDTO.PitchId), User = applicationContext.Users.Where(u => u.OpenId == openId).FirstOrDefault() };
+            Review newReview = new Review()
+            {
+                Message = reviewDTO.Message,
+                Status = reviewDTO.Status,
+                Pitch = applicationContext.Pitches.Include(p => p.BadgeLevel).ThenInclude(bl => bl.Badge).FirstOrDefault(p => p.PitchId == reviewDTO.PitchId),
+                User = applicationContext.Users.Include(u => u.UserLevels).ThenInclude(ul => ul.Badgelevel).ThenInclude(bl => bl.Badge).Where(u => u.OpenId == openId).FirstOrDefault()
+            };
+            
+            if(newReview.Pitch == null)
+            {
+                throw new Exception("The provided pitch does not exist.");
+            }
+
+            BadgeLevel badgeLevelOfPitch = newReview.Pitch.BadgeLevel;
+            BadgeLevel badgeLevelOfReviewer = newReview.User.UserLevels.Select(ul => ul.Badgelevel).FirstOrDefault(bl => bl.Badge.Name == badgeLevelOfPitch.Badge.Name);
+
+            if (badgeLevelOfPitch == null || badgeLevelOfReviewer == null || badgeLevelOfPitch.Level > badgeLevelOfReviewer.Level)
+            {
+                throw new Exception("You are not allowed to give a review.");
+            }
             applicationContext.Add(newReview);
             applicationContext.SaveChanges();
         }
