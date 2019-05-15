d61a43bf53becfb48cd36ede23a14062a71e201a
Botond Balogh <botond.balogh8@gmail.com>
2019-05-15 10:07:20 +0200

    Add empty unittest

---
+++ ASP.NET-Core-Webapp.UnitTests/EmptyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ASP.NET_Core_Webapp.UnitTests
+{
+    public class EmptyTest
+    {
+        [Fact]
+        public void Simpletest()
+        {
+            int n = 5;
+            int result = 5;
+            Assert.Equal(result, n);
+        }
+    }
+}
