10c0b27cd37ef4becc6f7c79358364b4e06f7488
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-16 09:17:17 +0200

    PostReviewTest.cs/ReviewController.cs/ReviewService.cs modify

---
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PutReviewTest.cs
@@ -0,0 +1,70 @@
+﻿using ASP.NET_Core_Webapp.DTO;
+using ASP.NET_Core_Webapp.IntegrationTests.Fixtures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
+{
+    [Collection("BaseCollection")]
+    public class PutReviewTest
+    {
+        private readonly TestContext testContext;
+
+        public PutReviewTest(TestContext testContext)
+        {
+            this.testContext = testContext;
+        }
+
+        [Fact]
+        public async Task PutReview_NoBodyContent_ShouldReturn404()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Put, "/review");
+            request.Content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PustReview_AllFielsdFilledReviewerOwnsBadge_ShouldReturn201()
+        {
+            ReviewDTO review = new ReviewDTO() { Message = "testreview", Status = true, PitchId = 3 };
+
+            var request = new HttpRequestMessage(HttpMethod.Put, "/review");
+            request.Content = (new StringContent(JsonConvert.SerializeObject(review), Encoding.UTF8, "application/json"));
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PutReview_AllFielsdFilledSelfReview_ShouldReturn401()
+        {
+            ReviewDTO review = new ReviewDTO() { Message = "testreview", Status = true, PitchId = 5 };
+
+            var request = new HttpRequestMessage(HttpMethod.Put, "/review");
+            request.Content = (new StringContent(JsonConvert.SerializeObject(review), Encoding.UTF8, "application/json"));
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PutReview_AllFielsdFilledPithDoesNotExist_ShouldReturn404()
+        {
+            ReviewDTO review = new ReviewDTO() { Message = "testreview", Status = true, PitchId = 8 };
+
+            var request = new HttpRequestMessage(HttpMethod.Put, "/review");
+            request.Content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
+
+            var response = await testContext.Client.SendAsync(request);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
--- ASP.NET-Core-Webapp/Services/ReviewService.cs
+++ ASP.NET-Core-Webapp/Services/ReviewService.cs
@@ -1,10 +1,9 @@
 ﻿using ASP.NET_Core_Webapp.Data;
 using ASP.NET_Core_Webapp.DTO;
 using ASP.NET_Core_Webapp.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Webapp.Services
 {
@@ -19,14 +18,73 @@ namespace ASP.NET_Core_Webapp.Services
 
         public void CreateReview(string openId, ReviewDTO reviewDTO)
         {
-            Review newReview = new Review() { Message = reviewDTO.Message, Status = reviewDTO.Status, Pitch = applicationContext.Pitches.FirstOrDefault(p => p.PitchId == reviewDTO.PitchId), User = applicationContext.Users.Where(u => u.OpenId == openId).FirstOrDefault() };
+            Review newReview = new Review()
+            {
+                Message = reviewDTO.Message,
+                Status = reviewDTO.Status,
+                Pitch = applicationContext.Pitches
+                                            .Include(p => p.BadgeLevel)
+                                            .ThenInclude(bl => bl.Badge)
+                                            .FirstOrDefault(p => p.PitchId == reviewDTO.PitchId),
+                User = applicationContext.Users
+                                            .Include(u => u.UserLevels)
+                                            .ThenInclude(ul => ul.Badgelevel)
+                                            .ThenInclude(bl => bl.Badge)
+                                            .FirstOrDefault(u => u.OpenId == openId)
+            };
+
+            if (newReview.Pitch == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            BadgeLevel badgeLevelOfPitch = newReview.Pitch.BadgeLevel;
+            BadgeLevel badgeLevelOfReviewer = newReview.User.UserLevels
+                                                                .Select(ul => ul.Badgelevel)
+                                                                .FirstOrDefault(bl => bl.Badge.Name.ToLower().Contains(badgeLevelOfPitch.Badge.Name.ToLower()));
+
+            if (badgeLevelOfPitch == null || badgeLevelOfReviewer == null || badgeLevelOfPitch.Badge.Name != badgeLevelOfReviewer.Badge.Name || newReview.User == newReview.Pitch.User)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
             applicationContext.Add(newReview);
             applicationContext.SaveChanges();
         }
 
         public void UpdateReview(string openId, ReviewDTO reviewDTO, long id)
         {
-            Review updatedReview = new Review() { ReviewId = id, Message = reviewDTO.Message, Status = reviewDTO.Status, Pitch = applicationContext.Pitches.FirstOrDefault(p => p.PitchId == reviewDTO.PitchId), User = applicationContext.Users.Where(u => u.OpenId == openId).FirstOrDefault() };
+            Review originalReview = applicationContext.Reviews.Include(r => r.User).FirstOrDefault(r => r.ReviewId == id);
+            if (originalReview == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Review updatedReview = new Review()
+            {
+                ReviewId = id,
+                Message = reviewDTO.Message,
+                Status = reviewDTO.Status,
+                Pitch = applicationContext.Pitches
+                                            .Include(p => p.BadgeLevel)
+                                            .ThenInclude(bl => bl.Badge)
+                                            .FirstOrDefault(p => p.PitchId == reviewDTO.PitchId),
+                User = applicationContext.Users
+                                            .Include(u => u.UserLevels)
+                                            .ThenInclude(ul => ul.Badgelevel)
+                                            .ThenInclude(bl => bl.Badge)
+                                            .FirstOrDefault(u => u.OpenId == openId)
+            };
+
+            if (updatedReview.Pitch == null)
+            {
+                throw new NullReferenceException();
+            }
+            if (updatedReview.User != originalReview.User)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
             applicationContext.Update(updatedReview);
             applicationContext.SaveChanges();
         }
