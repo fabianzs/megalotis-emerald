86f26d3cc967dd0bd14b44a0804f6ee3fe1622da
Botond Balogh <botond.balogh8@gmail.com>
2019-05-16 16:41:24 +0200

    Add web.config

--- ASP.NET-Core-Webapp.IntegrationTests/Fixtures/TestContext.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Fixtures/TestContext.cs
@@ -10,7 +10,6 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Fixtures
     {
         private TestServer server;
         public HttpClient Client { get; set; }
-        public IAuthService AuthServiceContext { get; set; }
 
         public TestContext()
         {
@@ -20,7 +19,6 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Fixtures
 
             server = new TestServer(builder);
             Client = server.CreateClient();
-            AuthServiceContext = server.Host.Services.GetService(typeof(IAuthService)) as AuthService;
         }
 
         public void Dispose()
