ea58bc476122c549f663a5eba781f8f6502879b7
JohnLacklan <janos.f96@gmail.com>
2019-05-27 20:33:43 +0200

    Segits Zsofi

--- ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchControllerTest.cs
+++ ASP.NET-Core-Webapp.IntegrationTests/Scenarios/PitchControllerTest.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
 using Xunit;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
 {
@@ -60,13 +61,12 @@ namespace ASP.NET_Core_Webapp.IntegrationTests.Scenarios
         public async Task PitchUpdateTest()
         {
             List<Review> Holders = new List<Review>();
-            Pitch pitch = new Pitch(new Badge("kaka"), 2, 3, "English speaker", Holders);
-            pitch.User = testContext.context.Users.FirstOrDefault(u => u.Name == "balazs.barna");
-            testContext.context.Add(pitch);
-            testContext.context.SaveChanges();
+            Pitch pitch = testContext.context.Pitches.Include(p => p.User).FirstOrDefault(t => t.PitchId == 1);
+            //pitch.User = testContext.context.Users.FirstOrDefault(u => u.Name == "balazs.barna");
+            
 
-            var request = new HttpRequestMessage(HttpMethod.Post, "/pitch");
-            var response = await testContext.Client.PostAsync("/pitch", new StringContent(JsonConvert.SerializeObject(pitch), Encoding.UTF8, "application/json"));
+            var request = new HttpRequestMessage(HttpMethod.Put, "/pitch");
+            var response = await testContext.Client.PutAsync("/pitch", new StringContent(JsonConvert.SerializeObject(pitch), Encoding.UTF8, "application/json"));
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
